ce7eab61e40a189b5125cba53adffe32b982fa19
Hevesi Istvan <hevesiistvan71@hotmail.com>
2022-07-04 12:46:04 +0200

    better-highlight.directive.ts létrehozása a better-highlight mappába.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
